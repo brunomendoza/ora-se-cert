dfcd9f8c16d5ba80ff88ca3af04dc050c2839353
Bruno Mendoza <hola@brunomendoza.es>
2021-03-10 19:40:49 +0000

    Very first commmit. Practice 4.1 done. Multiple class were created. It was added accessors to 'Product' class. 'Shop' class serves as program launcher.

---
+++ ora-pr-4/src/ora/demo/Shop.java
@@ -0,0 +1,34 @@
+ /*
+  * Copyright (C) 2021  Bruno Mendoza
+  * 
+  * This program is free software: you can redistribute it and/or modify
+  * it under the terms of the GNU General Public License as published by
+  * the Free Software Foundation, either version 3 of the License, or
+  * (at your option) any later version.
+  * 
+  * This program is distributed in the hope that it will be useful,
+  * but WITHOUT ANY WARRANTY; without even the implied warranty of
+  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  * GNU General Public License for more details.
+  * 
+  * You should have received a copy of the GNU General Public License
+  * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+  */
+package ora.demo;
+
+import java.math.BigDecimal;
+
+/**
+ * {@code Shop} class represents an application that manages Products.
+ * @version 1.0
+ * @author Bruno Mendoza
+ */
+public class Shop {
+	public static void main(String[] args) {
+		Product p1  = new Product();
+		p1.setId(101);
+		p1.setName("Tea");
+		p1.setPrice(BigDecimal.valueOf(1.99));
+		System.out.println(p1.getId()+" "+p1.getName()+" "+p1.getPrice()+" "+p1.getDiscount());
+	}
+}

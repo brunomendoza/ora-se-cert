44afad10b16438bc0416206e2dfe8ac000eb6a1a
Bruno Mendoza <hola@brunomendoza.es>
2021-03-11 05:15:45 +0000

    Praction 6-1 done.
    It was created new classes extending class 'Product' and writed some
    constructors for this classes.
    
    We modified class 'Shop' to use this new classes.
    
    There was some errors related to the name of the packages. This issues
    were fixed.

--- ora-pr-4/src/ora/demo/app/Shop.java
+++ ora-pr-4/src/ora/demo/app/Shop.java
@@ -14,12 +14,16 @@
   * You should have received a copy of the GNU General Public License
   * along with this program.  If not, see <https://www.gnu.org/licenses/>.
   */
+
 package ora.demo.app;
 
 import java.math.BigDecimal;
+import java.time.LocalDate;
 
-import ora.demo.lab.Product;
-import ora.demo.lab.Rating;
+import ora.demo.data.Drink;
+import ora.demo.data.Food;
+import ora.demo.data.Product;
+import ora.demo.data.Rating;
 
 /**
  * {@code Shop} class represents an application that manages Products.
@@ -29,8 +33,8 @@ import ora.demo.lab.Rating;
 public class Shop {
 	public static void main(String[] args) {
 		Product p1  = new Product(101, "Tea", BigDecimal.valueOf(1.99));
-		Product p2 = new Product(102, "Coffee", BigDecimal.valueOf(1.99), Rating.FOUR_STAR);
-		Product p3 = new Product(103, "Cake", BigDecimal.valueOf(3.99), Rating.FIVE_STAR);
+		Product p2 = new Drink(102, "Coffee", BigDecimal.valueOf(1.99), Rating.FOUR_STAR);
+		Product p3 = new Food(103, "Cake", BigDecimal.valueOf(3.99), Rating.FIVE_STAR, LocalDate.now().plusDays(2));
 		Product p4 = new Product();
 		Product p5 = p3.applyRating(Rating.THREE_STAR);
 		

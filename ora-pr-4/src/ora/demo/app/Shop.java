0e75e30634308b4c27f1c326146fa4f97ca81039
Bruno Mendoza <hola@brunomendoza.es>
2021-03-12 23:05:48 +0000

    Activity 8-1 done.
    A new system for collecting reviews was added. This new system doesn't
    allow to associate reviews to a product but it can be used after a
    product was created to simulate a real review system.

--- ora-pr-4/src/ora/demo/app/Shop.java
+++ ora-pr-4/src/ora/demo/app/Shop.java
@@ -31,6 +31,7 @@ import ora.demo.data.Rating;
  * @author Bruno Mendoza
  */
 public class Shop {
+	
 	public static void main(String[] args) {
 		ProductManager pm = new ProductManager(new Locale("es", "ES"));
 		
@@ -38,36 +39,12 @@ public class Shop {
 		pm.printProductReport();
 		
 		p1 = pm.reviewProduct(p1, Rating.FOUR_STAR, "Nice hot cup of tea");
-		pm.printProductReport();
+		p1 = pm.reviewProduct(p1, Rating.TWO_STAR, "Rather weak tea");
+		p1 = pm.reviewProduct(p1, Rating.FOUR_STAR, "Fine tea");
+		p1 = pm.reviewProduct(p1, Rating.FOUR_STAR, "Good tea");
+		p1 = pm.reviewProduct(p1, Rating.FIVE_STAR, "Perfect tea");
+		p1 = pm.reviewProduct(p1, Rating.THREE_STAR, "Just add some lemon");
 		
-//		Product p2 = pm.createProduct(102, "Coffee", BigDecimal.valueOf(1.99), Rating.FOUR_STAR);
-//		Product p3 = pm.createProduct(103, "Cake", BigDecimal.valueOf(3.99), Rating.FIVE_STAR, LocalDate.now().plusDays(2));
-//		Product p4 = pm.createProduct(105, "Cookie", BigDecimal.valueOf(3.99), Rating.TWO_STAR, LocalDate.now());
-//		Product p5 = p3.applyRating(Rating.THREE_STAR);
-//		
-//		Product p8 = p4.applyRating(Rating.FIVE_STAR);
-//		Product p9 = p1.applyRating(Rating.TWO_STAR);
-//		
-//		Product p6 = pm.createProduct(104, "Chocolate", BigDecimal.valueOf(2.99), Rating.FIVE_STAR);
-//		Product p7 = pm.createProduct(104, new String("Chocolate"), BigDecimal.valueOf(2.99), Rating.FIVE_STAR, LocalDate.now().plusDays(2));
-//		System.out.println(p6.equals(p7));
-//		
-////		if (p3 instanceof Food) {
-////			LocalDate bestBefore = ((Food)p3).getBestBefore();
-////		}
-//		
-//		System.out.println(p1);
-//		System.out.println(p2);
-//		System.out.println(p3);
-//		System.out.println(p4);
-//		System.out.println(p5);
-//		System.out.println(p6);
-//		System.out.println(p7);
-//		
-//		System.out.println(p8);
-//		System.out.println(p9);
-//		
-//		System.out.println(p1.getBestBefore());
-//		System.out.println(p3.getBestBefore());
+		pm.printProductReport();
 	}
 }
--- ora-pr-4/src/ora/demo/data/ProductManager.java
+++ ora-pr-4/src/ora/demo/data/ProductManager.java
@@ -23,13 +23,14 @@ import java.text.NumberFormat;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.time.format.FormatStyle;
+import java.util.Arrays;
 import java.util.Locale;
 import java.util.ResourceBundle;
 
 public class ProductManager {
 	
 	private Product product;
-	private Review review;
+	private Review[] reviews = new Review[5];
 	
 	private Locale locale;
 	private ResourceBundle resources;
@@ -59,8 +60,25 @@ public class ProductManager {
 	}
 	
 	public Product reviewProduct(Product product, Rating rating, String comments) {
-		review = new Review(rating, comments);
-		this.product = product.applyRating(rating);
+		if (reviews[reviews.length - 1] != null) {
+			reviews = Arrays.copyOf(reviews, reviews.length + 5);
+		}
+		
+		int sum = 0;	
+		int i = 0;
+		boolean isReviewed = false;
+		
+		while (i < reviews.length && !isReviewed) {
+			if(reviews[i] == null) {
+				reviews[i] = new Review(rating, comments);
+				isReviewed = true;
+			}
+			
+			sum += reviews[i].getRating().ordinal();
+			i++;
+		}
+		
+		this.product = product.applyRating(Rateable.convert(Math.round((float)sum / i)));
 		return this.product;
 	}
 	
@@ -74,15 +92,23 @@ public class ProductManager {
 				dateFormat.format(product.getBestBefore())));
 		txt.append("\n");
 		
-		if (review != null) {
+		for (Review review : reviews) {
+			if (review == null) {
+				break;
+			}
+			
 			txt.append(MessageFormat.format(resources.getString("review"),
 					review.getRating().getStars(),
 					review.getComments()));
-		} else {
-			txt.append(resources.getString("no.reviews"));
+			
+			txt.append("\n");
 		}
 		
-		txt.append("\n");
+		if (reviews[0] == null) {
+			txt.append(resources.getString("no.reviews"));
+			txt.append("\n");
+		}
+		
 		System.out.println(txt);
 	}
 }
